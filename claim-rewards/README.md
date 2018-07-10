6a04a0f6528a8e8275728b58271174847720461d
Pete Cheyne <pete.cheyne@gmail.com>
2018-07-10 07:37:40 +0100

    Adds in the new claim rewards fetcher.

---
+++ claim-rewards/README.md
@@ -0,0 +1,54 @@
+## Automating `claimrewards`
+
+If you are in the fortunate position of being a paid EOS BP, you have to manually request the BP rewards every 24 hours + 1 second `¯\_(ツ)_/¯`
+
+This poses problems on many levels so we need to automate it. However, to perform a `claimrewards` you need to have access to an unlocked wallet, and that requires a plaintext password!
+
+This implementation is the best known mix between security and automation. It is not perfect, but its the best we've got!
+
+### Dependencies
+
+None, just sexy bash.
+
+### Create your claimer account
+
+As an added layer of security, we can create a separate subaccount and wallet just for actioning the `claimrewards`. The worst thing that could happen if this was compromised would be that a kind hacker could claim the rewards manually for you `ಠ‿ಠ`.
+
+In these examples, replace `blockmatrix` for your own producer name.
+
+- Create a new public/private key pair
+
+```
+cleos create key
+```
+
+- Create a new wallet and save the wallet password
+
+```
+cleos wallet create -n claims
+```
+
+- Import the private key created earlier
+
+```
+cleos wallet import CLAIM_PRIVATE_KEY -n claims
+```
+
+- Activate the `claimrewards` action for this new account
+
+```
+cleos set account permission blockmatrix claims '{"threshold":1,"keys":[{"key":"CLAIM_PUBLIC_KEY","weight":1}]}' "active" -p blockmatrix@active
+cleos set action permission blockmatrix eosio claimrewards claims
+```
+
+### Running
+
+Pass the wallet password as a parameter to the script, just so you arent checking that into version control.
+
+Update the parameters at the top of the script, and there are some optional parameters which you might want to change so that you can receive a shiny Slack alert detailing your rewards payout.
+
+To run the daemon, simply execute:
+
+```
+./claim_rewards PW5JfDojLFSmMTJfDLwQE5zvE4mjSBDwUpfuZWmws5Ecm4AF2StjX
+```
---
+++ claim-rewards/claim_rewards.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+###################################################################################################
+#
+# This script will auto claim BP rewards
+#
+# The premise is based on a safe "claim" only account permission
+#
+# Please refer to https://github.com/BlockMatrixNetwork/eos-bp-failover/tree/master/claim-rewards
+# for more info on how to set this up
+#
+# Example call:
+#
+# ./claim_rewards.sh PW5JfDojLFSmMTJfDLwQE5zvE4mjSBDwUpfuZWmws5Ecm4AF2StjW
+#
+# Update SLACK_WEBHOOK if you want to be notified via an incoming webhook
+#
+# Made with <3 by Pete @ Block Matrix
+#
+# See more EOS nerd shenanigans @ https://github.com/BlockMatrixNetwork
+#
+###################################################################################################
+
+# Update these params for your system
+CLEOS=/opt/eos/build/programs/cleos/cleos
+API=http://127.0.0.1:8888
+WALLET=http://127.0.0.1:55554
+PRODUCER=blockmatrix
+
+# Pass in the password so you arent checking it into a repo
+CLAIM_WALLET_PASS=$1
+
+# Only update these params if you know what you are doing
+SLACK_WEBHOOK=
+SLACK_CHANNEL="#notifications"
+CLAIM_PERMISSION=claims
+DIFF_CHECK=10
+EXEC="$CLEOS -u $API --wallet-url $WALLET"
+
+# Optional slack notification
+function notify()
+{
+    [[ ! -z $SLACK_WEBHOOK ]] && curl -s -X POST --data-urlencode "payload={\"channel\": \"$SLACK_CHANNEL\", \"username\": \"EOS Bot\", \"text\": \"$1\", \"icon_emoji\": \":ghost:\"}" $SLACK_WEBHOOK > /dev/null 2>&1
+}
+
+# Validate the password format
+if [[ ! $CLAIM_WALLET_PASS =~ ^PW5.* ]]; then
+    echo "Invalid wallet password"
+    exit 1
+fi
+
+# Unlock the wallet
+$EXEC wallet lock_all > /dev/null 2>&1
+$EXEC wallet unlock -n $CLAIM_PERMISSION --password $CLAIM_WALLET_PASS > /dev/null 2>&1
+
+if [ $? -ne 0 ]; then
+    echo "Incorrect wallet password"
+    exit 1
+fi
+
+# Fetch the last claim time for the producer and validate
+LAST_CLAIM=$($EXEC get table eosio eosio producers -l 10000 | grep -A 6 $PRODUCER | grep last_claim_time | grep -o '[[:digit:]]*')
+
+if [[ $? -ne 0 || $LAST_CLAIM -eq 0 ]]; then
+    echo "Invalid last claim time, claim manually to set a relevant time"
+    exit 1;
+fi
+
+# Calculate diff
+CLAIM_TIME=$(($LAST_CLAIM / 1000000))
+NOW=$(date +"%s")
+DIFF="$(($NOW-$CLAIM_TIME))"
+
+# Check if the diff exceeds our target
+if [ $DIFF -lt $DIFF_CHECK ]; then
+    echo "Not claiming rewards as $DIFF is less than $DIFF_CHECK"
+    exit 1;
+fi
+
+# Now we can claim rewards
+$EXEC wallet unlock -n $CLAIM_PERMISSION --password $CLAIM_WALLET_PASS > /dev/null 2>&1
+RESULT=$(($EXEC push action eosio claimrewards "[\"$PRODUCER\"]" -p $PRODUCER@$CLAIM_PERMISSION) 2>&1)
+
+# Notify via slack
+if [ $? -eq 0 ]; then
+    BPAY=$(echo "$RESULT" | grep "$PRODUCER <= eosio.token::transfer" | grep "eosio.bpay" | grep -Eo "[0-9]+\.[0-9]+ EOS")
+    VPAY=$(echo "$RESULT" | grep "$PRODUCER <= eosio.token::transfer" | grep "eosio.vpay" | grep -Eo "[0-9]+\.[0-9]+ EOS")
+    notify "Successfully claimed rewards: \`\`\`Block Pay: $BPAY\nVote Pay: $VPAY\`\`\`"
+else
+    notify "$(echo "Claim rewards failed with: \`\`\`$RESULT\`\`\`" | sed 's/\x1B\[[0-9;]\+[A-Za-z]//g')"
+fi
+
+# Raw output
+echo "$RESULT"
