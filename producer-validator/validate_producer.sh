845b6df2c142630d83b90266c4afdadf05ca167b
Pete Cheyne <pete.cheyne@gmail.com>
2018-08-11 11:30:06 +0100

    Integrate optional chat ops.

--- producer-validator/validate_producer.sh
+++ producer-validator/validate_producer.sh
@@ -2,11 +2,12 @@
 
 ###################################################################################################
 #
-# This script monitors that defined producers are producing the correct number of blocks in the schedule
+# This script monitors that defined producers are producing the correct number of blocks in each
+# round and it also detects negative latency
 #
-# It is intended to be used with Slack chatops
+# It is intended to be used with Slack chatops but outputa to stdout by default
 #
-# Please refer to https://github.com/BlockMatrixNetwork/eos-bp-failover/tree/master/external-rpc-api-monitor
+# Please refer to https://github.com/BlockMatrixNetwork/eos-bp-failover/tree/master/producer-validator
 # for more info on how to set this up
 #
 # Example call:
@@ -14,6 +15,8 @@
 # ./validate_producer.sh
 #
 # Update APIS to include the producers you want to monitor
+#
+# Update NODEOS_LOG to point to your local nodeos log file
 #
 # Update SLACK_WEBHOOK to your own incoming webhook
 #
@@ -26,14 +29,19 @@
 # Update these for your own settings
 declare -A APIS=( [pete]=blockmatrix1 [jaechung]=hkeoshkeosbp [ankh2054]=eos42freedom [mike]=eosdacserver [igorls]=eosriobrazil [xebb]=eosswedenorg )
 ENDPOINT="https://eosapi.blockmatrix.network"
-LOG_LOC="/mnt/stderr.txt"
-SEARCH="/tmp/search.txt"
-FILTER="/tmp/filter.txt"
-SLACK_WEBHOOK=https://hooks.slack.com/services/replace_me
+NODEOS_LOG="/mnt/stderr.txt"
+
+# Optional slack notifications add incoming webhook url to activate it
+SLACK_WEBHOOK=
 SLACK_CHANNEL="#monitoring"
 
-# Create temp log
-tail -n 5000 $LOG_LOC | grep "Received block" > $SEARCH
+# Dont modify below here
+SEARCH="/tmp/search.txt"
+FILTER="/tmp/filter.txt"
+echo "" > $FILTER
+
+# Create temp log, assume non debug nodeos log
+tail -n 5000 $NODEOS_LOG | grep "Received block" > $SEARCH
 
 # Grab the top 21
 ACTIVE=$(curl -s "$ENDPOINT/v1/chain/get_producers" -X POST -d '{"json":true, "limit":21}' | jq '.rows')
@@ -47,10 +55,18 @@ while read line; do
     [[ $line > $LAST && $line < $NOW || $line =~ $NOW ]] && echo $line >> $FILTER
 done < $SEARCH
 
+# Handle notifications
+function notify()
+{
+    echo $1
+    [[ ! -z $SLACK_WEBHOOK ]] && curl -s -X POST --data-urlencode "payload={\"channel\": \"$SLACK_CHANNEL\", \"username\": \"EOS Bot\", \"text\": \"@$2 $1\", \"icon_emoji\": \":ghost:\"}" $SLACK_WEBHOOK > /dev/null 2>&1
+}
+
 # Check each producer
 for K in "${!APIS[@]}"
 do
     PRODUCER=$(echo $ACTIVE | jq --arg prd "${APIS[$K]}" '.[] | select(.owner == $prd)')
+    PASS=1
 
     if [ "$PRODUCER" == "" ]; then
         echo "${APIS[$K]} is not in the top 21"
@@ -60,16 +76,19 @@ do
     # First check is to check each producer hit their 6 second target
     COUNT=$(grep -c "${APIS[$K]}" $FILTER)
     if [[ "$COUNT" < "12" ]]; then
-        echo "${APIS[$K]} has produced less than 12 blocks: $COUNT"
+        notify "${APIS[$K]} has produced less than 12 blocks: $COUNT" $K
+        PASS=0
     fi
 
     # Second check is to see if there has been any negative latency
     NEG=$(grep "${APIS[$K]}" $FILTER | grep "latency: -")
     if [ $? -eq 0 ]; then
-        echo "${APIS[$K]} has negative latency: $NEG"
+        notify "${APIS[$K]} has negative latency: $NEG" $K
+        PASS=0
     fi
 
-    echo "${APIS[$K]} has 12 healthy blocks and no negative latency"
+    # Output if they passed all checks
+    if [ $PASS -eq 1 ]; then
+        echo "${APIS[$K]} has 12 healthy blocks and no negative latency"
+    fi
 done
-
-rm $FILTER $SEARCH
