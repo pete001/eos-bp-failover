742b8a34708fb84fdf06b5900ab60a2e040a61b9
Pete Cheyne <pete.cheyne@gmail.com>
2018-08-11 10:15:20 +0100

    Working version.

--- producer-validator/validate_producer.sh
+++ producer-validator/validate_producer.sh
@@ -1,56 +1,61 @@
 #!/bin/bash
 
-############################################################################################
+###################################################################################################
 #
-# This script is activated when keepalived wants to promote a new producer
+# This script monitors that defined producers are producing the correct number of blocks in the schedule
 #
-# The plan is to use the producer API to pause/resume progress based on the STATE
+# It is intended to be used with Slack chatops
 #
-# There is no provision to restart a downed nodeos process, its assumed this is catered for
+# Please refer to https://github.com/BlockMatrixNetwork/eos-bp-failover/tree/master/external-rpc-api-monitor
+# for more info on how to set this up
+#
+# Example call:
+#
+# ./validate_producer.sh
+#
+# Update APIS to include the producers you want to monitor
+#
+# Update SLACK_WEBHOOK to your own incoming webhook
 #
 # Made with <3 by Pete @ Block Matrix
 #
 # See more EOS nerd shenanigans @ https://github.com/BlockMatrixNetwork
 #
-############################################################################################
+###################################################################################################
 
-# Change these vars based on your node configuration
-NODEOS_HTTP=localhost
-NODEOS_PORT=8888
+# Update these for your own settings
+declare -A APIS=( [pete]=blockmatrix1 [jaechung]=hkeoshkeosbp [ankh2054]=eos42freedom [mike]=eosdacserver [igorls]=eosriobrazil [xebb]=eosswedenorg )
+ACTIVE=$(curl -s "https://eosapi.blockmatrix.network/v1/chain/get_producers" -X POST -d '{"json":true, "limit":21}' | jq '.rows')
+LOG_LOC="/mnt/stderr.txt"
+SEARCH="/tmp/search.txt"
+FILTER="/tmp/filter.txt"
 SLACK_WEBHOOK=https://hooks.slack.com/services/replace_me
+SLACK_CHANNEL="#monitoring"
 
-# These are sent via keepalived
-TYPE=$1
-NAME=$2
-STATE=$3
+# Create temp log
+tail -n 5000 $LOG_LOC | grep "Received block" > $SEARCH
 
-# Echo the result of the pause/resume curl and call slack if relevant
-# This could be switched out for any service such as Pager Duty, etc
-function notify()
-{
-    MESSAGE=$1
-    echo $MESSAGE
-    [[ ! -z $SLACK_WEBHOOK ]] && curl -s -X POST --data-urlencode "payload={\"channel\": \"#ops\", \"username\": \"keepalived\", \"text\": \"$MESSAGE\", \"icon_emoji\": \":ghost:\"}" $SLACK_WEBHOOK > /dev/null
-}
+# We only care about the last 2min 6sec
+LAST=$(date --date="-126 sec" "+%Y-%m-%dT%H:%M:%S")
+NOW=$(date "+%Y-%m-%dT%H:%M:%S")
 
-# Based on the state, perform the relevant action
-case $STATE in
-    "MASTER") RESULT=$(curl -s "$NODEOS_HTTP:$NODEOS_PORT/v1/producer/resume")
-              if [ "$RESULT" == "{\"result\":\"ok\"}" ]
-              then
-                  notify "$HOSTNAME successfully promoted to the primary producer"
-              else
-                  notify "$HOSTNAME failed to resume, investigate immediately"
-              fi
-              exit 0
-              ;;
-    "BACKUP") RESULT=$(curl -s "$NODEOS_HTTP:$NODEOS_PORT/v1/producer/pause")
-              if [ "$RESULT" == "{\"result\":\"ok\"}" ]
-              then
-                  notify "$HOSTNAME successfully relegated to secondary producer"
-              else
-                  notify "$HOSTNAME failed to pause, investigate immediately"
-              fi
-              exit 0
-              ;;
-esac
+# Create filtered log
+while read line; do
+    [[ $line > $LAST && $line < $NOW || $line =~ $NOW ]] && echo $line >> $FILTER
+done < $SEARCH
+
+# Do not change below here
+for K in "${!APIS[@]}"
+do
+    PRODUCER=$(echo $ACTIVE | jq --arg prd "${APIS[$K]}" '.[] | select(.owner == $prd)')
+
+    if [ "$PRODUCER" == "" ]; then
+        echo "${APIS[$K]} is not in the top 21"
+        continue
+    fi
+
+    COUNT=$(grep -c "${APIS[$K]}" $FILTER)
+    echo "${APIS[$K]} is a top 21 baller, count of blocks: $COUNT"
+done
+
+rm $FILTER $SEARCH
